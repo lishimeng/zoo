de63df120b6edcd3d8cab3b2f54cc484af0fcc23
alex.dou <alex.dou@qq.com>
2023-06-27 22:00:14 +0800

    页面cache

--- cmd/home/api/application.go
+++ cmd/home/api/application.go
@@ -3,13 +3,16 @@ package api
 import (
 	"github.com/kataras/iris/v12"
 	"github.com/lishimeng/home/cmd/home/midware"
+	"github.com/lishimeng/home/internal/etc"
 	"github.com/lishimeng/home/static"
 	"time"
 )
 
 func Application(app *iris.Application) {
 
-	app.Use(iris.Cache304(time.Second * 10))
+	if etc.Config.Web.Cache > 0 { // 设置了页面cache
+		app.Use(iris.Cache304(time.Hour * time.Duration(etc.Config.Web.Cache)))
+	}
 
 	engine := iris.HTML(static.Static, ".html")
 	engine.AddFunc("indent", midware.Indent)
--- internal/etc/config.models.go
+++ internal/etc/config.models.go
@@ -2,18 +2,19 @@ package etc
 
 type Configuration struct {
 	Web web `toml:"web"`
-	Db  db
+	Db  db  `toml:"db"`
 }
 
 type web struct {
 	Listen string `toml:"listen"`
+	Cache  int    `toml:"cache"`
 }
 
 type db struct {
-	User     string
-	Password string
-	Host     string
-	Port     int
-	Database string
-	Ssl      string
+	User     string `toml:"user"`
+	Password string `toml:"password"`
+	Host     string `toml:"host"`
+	Port     int    `toml:"port"`
+	Database string `toml:"database"`
+	Ssl      string `toml:"ssl"`
 }
